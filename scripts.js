7e53a8e2df749a8bece023b4e7dcf483c9c47915
Lxrcade <167519963+Lxrcade@users.noreply.github.com>
2024-10-24 18:42:23 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
-let lastScrollTop = 0; // Variable to keep track of the last scroll position
-const nav = document.querySelector('.navc'); // Select the navigation bar
+window.onscroll = function() {
+  var navbar = document.querySelector('.navc');
+  var sticky = navbar.offsetTop;
 
-window.addEventListener('scroll', function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (scrollTop > lastScrollTop) {
-        // Scrolling down
-        nav.style.top = "-100px"; // Hide the navbar
-    } else {
-        // Scrolling up
-        nav.style.top = "0"; // Show the navbar
-    }
-    lastScrollTop = scrollTop; // Update last scroll position
-});
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add('fixed-top');  // Add the fixed class on scroll
+  } else {
+    navbar.classList.remove('fixed-top');  // Remove when scrolled back up
+  }
+};
