0a96528cdeae305e4774f74d2252c7458fab61ae
Lxrcade <167519963+Lxrcade@users.noreply.github.com>
2024-11-24 16:00:53 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,10 +22,10 @@ window.onscroll = function() {
  // JavaScript to Open and Close the Modal
     function openModal() {
       document.getElementById("modal2").classList.add("show");
-      document.getElementById("backdrop").classList.add("show");
+      document.getElementById("backdrop2").classList.add("show");
     }
 
     function closeModal() {
       document.getElementById("modal2").classList.remove("show");
-      document.getElementById("backdrop").classList.remove("show");
+      document.getElementById("backdrop2").classList.remove("show");
     }
